8e682ad09d481e9b4682d2b494ee7b71119826ca
AnastasiyaMAV <mihalukova112@mail.ru>
2022-01-24 22:15:50 +0200

    сервер принимает форму, оправила поля для отправки на сервер

--- findNannyForm/index.js
+++ findNannyForm/index.js
@@ -1,9 +1,9 @@
 function showOther() {
-  document.getElementById("other").style.visibility = "visible";
+  document.getElementById("myschedule").style.visibility = "visible";
 }
 
 function noshowOther() {
-  document.getElementById("other").style.visibility = "hidden";
+  document.getElementById("myschedule").style.visibility = "hidden";
 }
 
 back = () => {
@@ -24,7 +24,7 @@ validate = () => {
   let phoneFormat = /^((8|\+7)[\- ]?)?(\(?\d{3,4}\)?[\- ]?)?[\d\- ]{5,10}$/;
   let indexFormat = /^\d{6}$/;
 
-  let a = document.forms["nannyForm"]["name"].value;
+  let a = document.forms["nannyForm"]["Name"].value;
   if(a == "") {
     document.getElementById('errName').textContent="Обязательно для заполнения";
     document.getElementById('errNameBorder').classList.add('borderErr');
@@ -34,7 +34,7 @@ validate = () => {
     document.getElementById('errNameBorder').classList.remove('borderErr');
   }
 
-  let b = document.forms["nannyForm"]["surname"].value;
+  let b = document.forms["nannyForm"]["LastName"].value;
   if(b == "") {
     document.getElementById('errSurname').textContent="Обязательно для заполнения";
     document.getElementById('errSurnameBorder').classList.add('borderErr');
@@ -44,47 +44,48 @@ validate = () => {
     document.getElementById('errSurnameBorder').classList.remove('borderErr');
   }
 
-  let с = document.forms["nannyForm"]["email"].value;
+  let с = document.forms["nannyForm"]["Email"].value;
   if(с == "") {
     document.getElementById('errEmail').textContent="Обязательно для заполнения";
     document.getElementById('errEmailBorder').classList.add('borderErr');
     return false;
-  } else if( !document.getElementById('email').value.match(mailFormat) ) {
+  } else if( !document.getElementById('Email').value.match(mailFormat) ) {
       document.getElementById('errEmail').textContent="Неверный формат Email. Пример: example-678@mail.com";
       document.getElementById('errEmailBorder').classList.add('borderErr');
-      document.getElementById('email').value= '';
+      document.getElementById('Email').value= '';
       return false;
   } else {
     document.getElementById('errEmail').textContent='';
     document.getElementById('errEmailBorder').classList.remove('borderErr');
   }
   
-  let d = document.forms["nannyForm"]["mobile"].value;
+  let d = document.forms["nannyForm"]["phone"].value;
   if(d == "") {
     document.getElementById('errMobile').textContent="Обязательно для заполнения";
     document.getElementById('errMobileBorder').classList.add('borderErr');
     return false;
-  } else if( !document.getElementById('mobile').value.match(phoneFormat) ) {
+  } else if( !document.getElementById('phone').value.match(phoneFormat) ) {
       document.getElementById('errMobile').textContent="Неверный формат телефона. Пример: +7 xxx xxx xx xx";
       document.getElementById('errMobileBorder').classList.add('borderErr');
-      document.getElementById('mobile').value = '';
+      document.getElementById('phone').value = '';
       return false;
   } else {
     document.getElementById('errMobile').textContent='';
     document.getElementById('errMobileBorder').classList.remove('borderErr');
   }
 
-  let f = document.forms["nannyForm"]["child"].value;
+  let f = document.forms["nannyForm"]["kids-name"].value;
   if (f == "") {
     document.getElementById('errChild').textContent="Обязательно для заполнения";
     document.getElementById('errChildBorder').classList.add('borderErr');
+    document.getElementById('kids-name').value = '';
     return false;
   } else {
     document.getElementById('errChild').textContent='';
     document.getElementById('errChildBorder').classList.remove('borderErr');
   }
 
-  let g = document.forms["nannyForm"]["birth"].value;
+  let g = document.forms["nannyForm"]["Date"].value;
   if (g == "") {
     document.getElementById('errBirth').textContent="Обязательно для заполнения";
     document.getElementById('errBirthBorder').classList.add('borderErr');
@@ -104,22 +105,22 @@ validate = () => {
     document.getElementById('errCityBorder').classList.remove('borderErr');
   }
 
-  let k = document.forms["nannyForm"]["index"].value;
+  let k = document.forms["nannyForm"]["post-index"].value;
   if (k == "") {
     document.getElementById('errIndex').textContent="Обязательно для заполнения";
     document.getElementById('errIndexBorder').classList.add('borderErr');
     return false;
-  } else if( !document.getElementById('index').value.match(indexFormat) ) {
+  } else if( !document.getElementById('post-index').value.match(indexFormat) ) {
     document.getElementById('errIndex').textContent="Неверный формат индекса. Пример: 123654";
     document.getElementById('errIndexBorder').classList.add('borderErr');
-    document.getElementById('index').value='';
+    document.getElementById('post-index').value='';
     return false;
   } else {
     document.getElementById('errIndex').textContent='';
     document.getElementById('errIndexBorder').classList.remove('borderErr');
   }
 
-  let l = document.forms["nannyForm"]["adress"].value;
+  let l = document.forms["nannyForm"]["address"].value;
   if (l == "") {
     document.getElementById('errAdress').textContent="Обязательно для заполнения";
     document.getElementById('errAdressBorder').classList.add('borderErr');
@@ -163,21 +164,18 @@ openCity = (event, cityName) => {
 }
 
 submitForm = (event) => {
+  // event.preventDefault();
   formData = new FormData(formElementQuestionnaire);
 
   for(let value of formData.values()) {
     console.log(value);
   }
   
-  // https://httpbin.org/post
   fetch('https://api.star-c.ru/webhook/mynanny', {
           method: 'POST',
           body: formData,
           mode: 'no-cors'
       })
-
-      .then(response => response.json())
-      // .then(response => response.text())
       
       .then(data => {
         console.log(data);
@@ -185,18 +183,6 @@ submitForm = (event) => {
         document.getElementById("applicationSent").classList.add("applicationSent");
         document.getElementById("tabCity").classList.add("formOff");
         document.getElementById("formElementQuestionnaire").classList.add("formOff");
-        alert("Ура");
-      //   console.log(data);
-      //   data 
-      //   ? (
-      //     console.log('true'),
-      //     JSON.parse(data),
-      //     document.getElementById("applicationSent").classList.remove("applicationSentOff"),
-      //     document.getElementById("applicationSent").classList.add("applicationSent"),
-      //     document.getElementById("tabCity").classList.add("formOff"),
-      //     document.getElementById("formElementQuestionnaire").classList.add("formOff"),
-      //     alert("Ура"))
-      //   : console.log('false');
     })
 
       .catch(error =>{
@@ -207,7 +193,6 @@ submitForm = (event) => {
           document.getElementById("errorSent").classList.add("errorSent");
           document.getElementById("tabCity").classList.add("formOff");
           document.getElementById("formElementQuestionnaire").classList.add("formOff");
-          alert("не Ура");
         }  
       })
 }
